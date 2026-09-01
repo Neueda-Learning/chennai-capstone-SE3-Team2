19870b65c041d72e69d3f3c1ee4e0d18f0f690c2
kalmeiudega <shreedharsherlekar2004@gmail.com>
2026-09-01 15:37:19 +0000

    Delete sprint-3 directory

--- sprint-3/seed/002_seed_instrument.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO instrument
-    (ticker, isin, trading_status)
-VALUES
-    ('TCS', 'INE467B01029', 'TRADING'),
-
-    ('INFY', 'INE009A01021', 'TRADING'),
-
-    ('RELIANCE', 'INE002A01018', 'STOPPED');
--- sprint-3/seed/004_seed_position.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO position
-    (account_id, instrument_id, quantity, average_price)
-VALUES
-    (1, 1, 10, 3500.00),
-
-    (1, 2, 20, 1650.00);
