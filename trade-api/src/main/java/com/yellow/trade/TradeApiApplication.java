105aa3a30fa31bf4e033559b8cd77a407ae81670
Kezia <kezia.stephano7@gmail.com>
2026-09-09 22:44:37 +0530

    [SEC3-214]: Added dependencies required for integration testing

--- trade-api/src/main/java/com/yellow/trade/TradeApiApplication.java
+++ trade-api/src/main/java/com/yellow/trade/TradeApiApplication.java
@@ -1,9 +1,12 @@
 package com.yellow.trade;
 
+import org.mybatis.spring.annotation.MapperScan;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.yellow.trade.mappers")
 public class TradeApiApplication {
 
     public static void main(String[] args) {
